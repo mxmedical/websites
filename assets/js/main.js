a52f7aec25646930d3be57ef2f121290a5fce590
Furkan Titiz <65949007+titizfurkan@users.noreply.github.com>
2020-12-10 13:35:35 +0300

    updatedjs

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,6 +25,7 @@
   | 10. Progress Bar
   | 11. Pricing Table
   | 12. Ajax Contact Form And Appointment
+  | 13. Mailchimp start
   | 14. Modal Video
   | 15. Light Gallery
   | 16. Tamjid Counter
@@ -493,6 +494,39 @@
         }
         return false;
       });
+    }
+  }
+
+  /*--------------------------------------------------------------
+    13. Mailchimp start
+  --------------------------------------------------------------*/
+  function mailchimpInit() {
+    if ($.exists('.mailchimp')) {
+      if ($('.mailchimp').length > 0) {
+        $('.mailchimp').ajaxChimp({
+          language: 'es',
+          callback: mailchimpCallback
+        });
+      }
+
+      function mailchimpCallback(resp) {
+        if (resp.result === 'success') {
+          $('.subscription-success').html('<i class="fa fa-check"></i><br/>' + resp.msg).fadeIn(1000);
+          $('.subscription-error').fadeOut(500);
+
+        } else if (resp.result === 'error') {
+          $('.subscription-error').html('<i class="fa fa-times"></i><br/>' + resp.msg).fadeIn(1000);
+        }
+      }
+      $.ajaxChimp.translations.es = {
+        'submit': 'Submitting...',
+        0: 'We have sent you a confirmation email',
+        1: 'Please enter a value',
+        2: 'An email address must contain a single @',
+        3: 'The domain portion of the email address is invalid (the portion after the @: )',
+        4: 'The username portion of the email address is invalid (the portion before the @: )',
+        5: 'This email address looks fake or invalid. Please enter a real email address'
+      };
     }
   }
 
